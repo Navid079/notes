c20be0dfce36d3992b9f607c3194f5c72fd19a36
Navid079 <navid.lee79@gmail.com>
2022-06-30 14:41:45 +0430

    validators added

--- routes.js
+++ routes.js
@@ -1,13 +1,34 @@
 const express = require('express');
 
 const controllers = require('./controllers');
+const validators = require('./validators');
 
 const router = express.Router();
 
-router.post('/addNote', controllers.addNote);
-router.get('/getNote', controllers.getNote);
+router.post(
+  '/addNote',
+  validators.addNote,
+  validators.compiler,
+  controllers.addNote
+);
+router.get(
+  '/getNote',
+  validators.getNote,
+  validators.compiler,
+  controllers.getNote
+);
 router.get('/getAllNotes', controllers.getAllNotes);
-router.put('/editNote', controllers.editNote);
-router.delete('/deleteNote', controllers.deleteNote);
+router.put(
+  '/editNote',
+  validators.editNote,
+  validators.compiler,
+  controllers.editNote
+);
+router.delete(
+  '/deleteNote',
+  validators.deleteNote,
+  validators.compiler,
+  controllers.deleteNote
+);
 
 module.exports = router;
---
+++ validators.js
@@ -0,0 +1,28 @@
+const { body, query } = require('express-validator');
+
+exports.addNote = [
+  body('title', '422-title missing').exists().isString(),
+  body('text', '422-text missing').exists().isString(),
+];
+
+exports.getNote = [query('id', '404-not found').exists()];
+
+exports.editNote = [
+  body('id', '404-not found').exists(),
+  body('title', '422-title missing').exists().isString(),
+  body('text', '422-text missing').exists().isString(),
+];
+
+exports.deleteNote = [body('id', '404-not found').exists()];
+
+exports.compiler = (req, res, next) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) return next();
+  const resultArray = result.array({ onlyFirstError: true });
+  const [status, message] = resultArray[0].msg.split('-');
+  try {
+    res.status(status).json({ message });
+  } catch {
+    res.status(500).send();
+  }
+};
